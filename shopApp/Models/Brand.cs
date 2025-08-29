1d59eed337d5c59f2603f68a1a2a4aa2760edf96
Taiyrbek Anarbekov <an.taku2455@gmail.com>
2025-08-29 21:14:06 +0600

    add class Brand

---
+++ shopApp/Models/Brand.cs
@@ -0,0 +1,9 @@
+namespace shopApp.Models;
+
+public class Brand
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    
+    public ICollection<Product> Products { get; set; } = new List<Product>();
+}
